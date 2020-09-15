98e862d339c0eecf1daeda3ffed817555265201e
Marcelo Ludovico <maludovico@msn.com>
2020-09-15 18:25:46 -0300

    Fix auto update for different tables

--- public/assets/main.js
+++ public/assets/main.js
@@ -3,7 +3,6 @@
   
   function confirmDel(event) {
     event.preventDefault();
-    //console.log(event.target.parentNode.href);
     let token = document.getElementsByName('_token')[0].value;
     if(confirm('Deseja realmente remover este item?')){
       let ajax = new XMLHttpRequest();
@@ -53,22 +52,20 @@
     return LocationsData;
   }
 
-  function updateTable() {
+  function updateProductsTable() {
     let table = document.querySelector('table tbody');
     let row;
     let productsData = getProducts();
     let locationsData = getLocations();
-    console.log(locationsData);
+    table.innerHTML = '';
     productsData.forEach(item=>{
-      console.log(item);
-      let idcell = document.createElement('td');
-      idcell.scope = 'col';
-      idcell.innerText = item.name;
-      let locationcell = document.createElement('td');
-      locationcell.scope = 'col';
-      locationcell.innerText = locationsData.filter((lData)=>{
-        lData.id == item.id_location
-      });
+      let nameCell = document.createElement('td');
+      nameCell.scope = 'col';
+      nameCell.innerText = item.name;
+      let locationCell = document.createElement('td');
+      locationCell.scope = 'col';
+      let currentLocation = locationsData.filter(lData=>lData.id == item.id_location);
+      locationCell.innerText = currentLocation[0].aisle+currentLocation[0].shelf+currentLocation[0].side;
       let editBtn = document.createElement('button');
       editBtn.className = 'btn btn-primary btn-sm pr-3 pl-3';
       editBtn.innerText = 'Editar';
@@ -80,21 +77,65 @@
       removeBtn.innerText = 'Remover';
       let removeA = document.createElement('a');
       removeA.href = '/products/' + item.id;
-      removeA.className = 'js-del';
+      removeA.className = 'js-del ml-1';
       removeA.appendChild(removeBtn);
       let actioncell = document.createElement('td');
       actioncell.scope = 'col';
       actioncell.appendChild(editA);
       actioncell.appendChild(removeA);
       row = document.createElement('tr');
-      row.appendChild(idcell);
-      row.appendChild(locationcell);
+      row.appendChild(nameCell);
+      row.appendChild(locationCell);
       row.appendChild(actioncell);
+      table.appendChild(row);
     });
-    table.appendChild(row);
+  }
+
+  function updateLocationsTable() {
+    let table = document.querySelector('table tbody');
+    let row;
+    let locationsData = getLocations();
+    table.innerHTML = '';
+    locationsData.forEach(location=>{
+      let aisleCell = document.createElement('td');
+      aisleCell.scope = 'col';
+      aisleCell.innerText = location.aisle;
+      let shelfCell = document.createElement('td');
+      shelfCell.scope = 'col';
+      shelfCell.innerText = location.shelf;
+      let sideCell = document.createElement('td');
+      sideCell.scope = 'col';
+      sideCell.innerText = location.side;
+      let editBtn = document.createElement('button');
+      editBtn.className = 'btn btn-primary btn-sm pr-3 pl-3';
+      editBtn.innerText = 'Editar';
+      let editA = document.createElement('a');
+      editA.href = window.location.href + '/' + location.id + '/edit';
+      editA.appendChild(editBtn);
+      let removeBtn = document.createElement('button');
+      removeBtn.className = 'btn btn-danger btn-sm';
+      removeBtn.innerText = 'Remover';
+      let removeA = document.createElement('a');
+      removeA.href = window.location.href + '/' + location.id;
+      removeA.className = 'js-del ml-1';
+      removeA.appendChild(removeBtn);
+      let actioncell = document.createElement('td');
+      actioncell.scope = 'col';
+      actioncell.appendChild(editA);
+      actioncell.appendChild(removeA);
+      row = document.createElement('tr');
+      row.appendChild(aisleCell);
+      row.appendChild(shelfCell);
+      row.appendChild(sideCell);
+      row.appendChild(actioncell);
+      table.appendChild(row);
+    });
   }
 
   if(document.querySelector('.table')){
-    setInterval(updateTable, 2000);
+    if(window.location.pathname === '/products')
+      setInterval(updateProductsTable, 2000);
+    if(window.location.pathname === '/locations')
+      setInterval(updateLocationsTable, 2000);
   }
 })(window, document);
